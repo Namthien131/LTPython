a9303a9b509d7fba499c98bb817a018cf151305b
Namthien1301 <Namthien131@gmail.com>
2026-05-19 11:38:21 +0700

    Bài 121 strobogrammatic n chữ số

---
+++ cau_121a_strobogrammatic_n_chu_so.py
@@ -0,0 +1,38 @@
+def sinh_strobogrammatic(n, tong_do_dai):
+
+    if n == 0:
+        return [""]
+
+    if n == 1:
+        return ["0", "1", "8"]
+
+    ds = sinh_strobogrammatic(n - 2, tong_do_dai)
+
+    ket_qua = []
+
+    for s in ds:
+
+        if n != tong_do_dai:
+            ket_qua.append("0" + s + "0")
+
+        ket_qua.append("1" + s + "1")
+        ket_qua.append("6" + s + "9")
+        ket_qua.append("8" + s + "8")
+        ket_qua.append("9" + s + "6")
+
+    return ket_qua
+
+
+n = int(input("Nhap n (2 <= n <= 10): "))
+
+if 2 <= n <= 10:
+
+    print(f"Cac so strobogrammatic gom {n} chu so:")
+
+    ket_qua = sinh_strobogrammatic(n, n)
+
+    for so in ket_qua:
+        print(so)
+
+else:
+    print("n khong hop le")
